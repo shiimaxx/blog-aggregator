0b03a4dc91235806ce98f3ac0ae9be74b1e8c2a9
shiimaxx <yoshima@shiimaxx.com>
2018-11-17 23:51:32 +0900

    Add handleEntries test

--- app_test.go
+++ app_test.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"sync"
 	"testing"
+	"time"
+
+	"github.com/shiimaxx/blog-aggregator/structs"
 )
 
 func TestHandleRoot(t *testing.T) {
@@ -28,6 +34,40 @@ func TestHandleRoot(t *testing.T) {
 	}
 }
 
-func TestHandleEntries(t *testing.T) {
-	// TODO: Write test
+func TestHandleEntries_CacheHit(t *testing.T) {
+	req, err := http.NewRequest("GET", "/api/v1/entries", nil)
+	if err != nil {
+		t.Fatal("NewRequest failed: ", err.Error())
+	}
+
+	rec := httptest.NewRecorder()
+
+	s := server{
+		logger: log.New(os.Stdout, "", log.Lshortfile),
+		config: config{
+			userID: "testuser",
+		},
+		cache: &memStorage{
+			items: make(map[string]item),
+			mu:    &sync.RWMutex{},
+		},
+	}
+	s.cache.Set("qiita", []structs.Entry{
+		{Title: "a", URL: "https://example.com/a", CreatedAt: now},
+		{Title: "b", URL: "https://example.com/b", CreatedAt: now.Add(1 * time.Hour)},
+		{Title: "c", URL: "https://example.com/c", CreatedAt: now.Add(2 * time.Hour)},
+	}, 60*time.Second)
+	s.cache.Set("hatenablog", []structs.Entry{
+		{Title: "d", URL: "https://example.com/d", CreatedAt: now.Add(3 * time.Hour)},
+		{Title: "e", URL: "https://example.com/e", CreatedAt: now.Add(4 * time.Hour)},
+		{Title: "f", URL: "https://example.com/f", CreatedAt: now.Add(5 * time.Hour)},
+	}, 60*time.Second)
+
+	handler := s.handleEntries()
+
+	handler.ServeHTTP(rec, req)
+
+	if got, want := rec.Code, http.StatusOK; got != want {
+		t.Fatalf("got %v, want %v", got, want)
+	}
 }
