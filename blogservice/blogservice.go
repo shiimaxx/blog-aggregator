0aba1d7952108e262ccfc6f11abe5e61a0f88806
shiimaxx <yoshima@shiimaxx.com>
2018-11-18 23:25:58 +0900

    Implement BlogService

---
+++ blogservice/blogservice.go
@@ -0,0 +1,40 @@
+package blogservice
+
+import (
+	"sync"
+
+	"github.com/shiimaxx/blog-aggregator/structs"
+	"golang.org/x/sync/errgroup"
+)
+
+type BlogService struct {
+	FetchFunc []func() ([]structs.Entry, error)
+}
+
+func (b *BlogService) Add(fn func() ([]structs.Entry, error)) {
+	b.FetchFunc = append(b.FetchFunc, fn)
+}
+
+func (b *BlogService) Fetch() ([]structs.Entry, error) {
+	eg := errgroup.Group{}
+	var entries []structs.Entry
+	var mu sync.Mutex
+	for _, fn := range b.FetchFunc {
+		eg.Go(func() error {
+			e, err := fn()
+			if err != nil {
+				return err
+			}
+			mu.Lock()
+			entries = append(entries, e...)
+			mu.Unlock()
+			return nil
+		})
+	}
+
+	if err := eg.Wait(); err != nil {
+		return nil, err
+	}
+
+	return entries, nil
+}
